c63dc3ed4e6b2e1d8e585a87e026137177bbfbfd
Kirill Kornilov <k-korni1ov@yandex.ru>
2024-03-17 22:43:14 +0400

    feat: makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -125,8 +125,20 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const obj = {};
+  const keys = Object.keys(lettersObject);
+  const elems = Object.values(lettersObject);
+  for (let i = 0; i < elems.length; i += 1) {
+    elems[i].forEach((pos) => {
+      obj[pos] = keys[i];
+    });
+  }
+  let temp = '';
+  for (let i = 0; i < Object.keys(obj).length; i += 1) {
+    temp += obj[i];
+  }
+  return temp;
 }
 
 /**
